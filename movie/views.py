96abdb13d31cee6eb99ce8802d518175c776c4cd
Sahian Salome <sahiangutierrez14@gmail.com>
2025-07-31 11:03:22 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse('<h1>Welcome to Home Page </h1>')
     # return render(request,'home.html')
-    return render(request,'home.html',{'name':'Greg Lim'})
+    return render(request,'home.html',{'name':'Sahian Salomé Gutiérrez Ossa'})
 
 def about(request):
      return HttpResponse('<h1>Welcome to About page </h1>')
