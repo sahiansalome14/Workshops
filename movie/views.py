d385c580637602a178320ead36c323be86c0bbeb
Sahian Salome <sahiangutierrez14@gmail.com>
2025-07-31 11:24:34 -0500

    Archivo views

--- movie/views.py
+++ movie/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse('<h1>Welcome to Home Page </h1>')
     # return render(request,'home.html')
-    return render(request,'home.html',{'name':'Sahian Salomé Gutiérrez Ossa'})
+    return render(request,'home.html',{'name':' Sahian Salomé Gutiérrez Ossa'})
 
 def about(request):
-     return HttpResponse('<h1>Welcome to About page </h1>')
+     #return HttpResponse('<h1>Welcome to About page </h1>')
+     return render(request,'about.html')
