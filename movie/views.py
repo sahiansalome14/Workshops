402f0b50031f6f3c1354d42b3483e6ec8e00699f
Sahian Salome <sahiangutierrez14@gmail.com>
2025-08-02 18:24:43 -0500

    busqueda de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
+
 def home(request):
     # return HttpResponse('<h1>Welcome to Home Page </h1>')
     # return render(request,'home.html')
-    return render(request,'home.html',{'name':' Sahian Salomé Gutiérrez Ossa'})
+    #return render(request,'home.html',{'name':' Sahian Salomé Gutiérrez Ossa'})
+    searchTerm=request.GET.get('searchMovie')
+    if searchTerm:
+         movies=Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies=Movie.objects.all()
+    return render(request,'home.html',{'searchTerm':searchTerm, 'movies':movies})
 
 def about(request):
      #return HttpResponse('<h1>Welcome to About page </h1>')
