bd88e55f1ae975c3c48662c170220becf2445b33
Sahian Salome <159633291+sahiansalome14@users.noreply.github.com>
2025-10-30 09:53:48 -0500

    Add allowed host

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-7lnjge7mhk0fa_^9r%&%00^e7h#nv+i#+!i$@$9ffju3(*5ow@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.205.229.254']
 
 
 # Application definition
