d520a2857a510db76c0913f00cf8af23ad4cf73a
Sahian Salome <159633291+sahiansalome14@users.noreply.github.com>
2025-11-02 12:42:29 -0500

    host

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-7lnjge7mhk0fa_^9r%&%00^e7h#nv+i#+!i$@$9ffju3(*5ow@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.205.229.254']
+ALLOWED_HOSTS = ['54.160.239.199']
 
 
 # Application definition
